f8b04a002f41a58a9281c8067f34154b3cd3b87a
yahya <ahmadyahya1800@gmail.com>
2022-07-27 12:54:04 +0100

    Type unions

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom";
-import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 const App = () => {
   return (
     <div>
-      <GuestList />
+      <UserSearch />
     </div>
   );
 };
---
+++ src/state/UserSearch.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+const users = [
+  { name: "Sarah", age: 20 },
+  { name: "Alex", age: 20 },
+  { name: "Micheal", age: 20 },
+];
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState<{ name: string; age: number | undefined }>();
+  const onClick = () => {
+    const foundUser = users.find((user) => user.name === name);
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Find User</button>
+      <>{user?.name}</>
+    </div>
+  );
+};
+
+export default UserSearch;
